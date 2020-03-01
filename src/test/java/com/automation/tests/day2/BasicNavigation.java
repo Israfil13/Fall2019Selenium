d842090113c43483e4a4e8412e055675d0c808ba
Israfil13 <hajiyevisrafil@gmail.com>
2020-03-01 14:33:48 -0600

    Created basic Navigation Class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -7,21 +7,50 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BasicNavigation {
     public static void main(String[] args)throws Exception {
         WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver")
 
         WebDriver driver=new ChromeDriver();
         driver.get("http://google.com");
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+
+        driver.navigate().to("http://amazon.com");
         Thread.sleep(3000);
 
-        String titlre=driver.getTitle();
-        String ecxpectedTitle="Google";
-        if (ecxpectedTitle.equals(titlre)) {
-
+        if (driver.getTitle().toLowerCase().contains("amazon")){
             System.out.println("Test Passed");
         }else{
             System.out.println("Test Failed");
         }
-        System.out.println("titlre = " + titlre);
+
+        driver.navigate().back();
+        Thread.sleep(3000);
+
+        verifyEquals(driver.getTitle(),"Amazon");
+
+        driver.navigate().refresh();
         driver.close();
+
+//        String titlre=driver.getTitle();
+//        String ecxpectedTitle="Google";
+//        if (ecxpectedTitle.equals(titlre)) {
+//
+//            System.out.println("Test Passed");
+//        }else{
+//            System.out.println("Test Failed");
+//        }
+//        System.out.println("titlre = " + titlre);
+
+
+    }
+
+
+    public static void verifyEquals(String arg1,String arg2){
+        if (arg1.equals(arg2)){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
 
     }
 }
