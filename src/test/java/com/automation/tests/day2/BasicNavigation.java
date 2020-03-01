69377aaaa59770332d39cb93113d89e01d74b53a
Israfil13 <hajiyevisrafil@gmail.com>
2020-03-01 12:53:51 -0600

    Created basic Navigation Class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -5,14 +5,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver=new ChromeDriver();
         driver.get("http://google.com");
+        Thread.sleep(3000);
 
+        String titlre=driver.getTitle();
+        String ecxpectedTitle="Google";
+        if (ecxpectedTitle.equals(titlre)) {
 
-
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+        System.out.println("titlre = " + titlre);
+        driver.close();
 
     }
 }
